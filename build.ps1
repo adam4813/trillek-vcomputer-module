2b278c31f201fcf5d8eddfade486e61b60521891
Adam Martin <adam.madram@gmail.com>
2015-01-30 00:38:47 -0600

    The solution file is in the build folder

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $env:GTEST_ROOT = $PWD.Path + "\gtest-1.7.0\"
 cmake -G "Visual Studio 12" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TOOLS_VCOMPUTER=False .
 
-msbuild VCOMPUTER.sln /p:Configuration=Debug /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+msbuild build\VCOMPUTER.sln /p:Configuration=Debug /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
 cd bin
 cd Debug
